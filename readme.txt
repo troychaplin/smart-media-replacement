943b8a1affdc49d4d998ca7ab770f794330b4e2e
Troy Chaplin <troy.chaplin@gmail.com>
2026-05-15 10:44:20 -0400

    bump version

--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors:      areziaal
 Tags:              media, replace, revisions, attachment, pdf
 Requires at least: 6.6
 Tested up to:      7.0
-Stable tag:        1.2.0
+Stable tag:        1.2.1
 Requires PHP:      7.4
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
--- smart-media-replacement.php
+++ smart-media-replacement.php
@@ -4,7 +4,7 @@
  * Description:       Replace media library files with revision tracking. On multisite, network-activate only.
  * Requires at least: 6.6
  * Requires PHP:      7.4
- * Version:           1.2.0
+ * Version:           1.2.1
  * Author:            Troy Chaplin
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
